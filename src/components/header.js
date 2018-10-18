e8edae4ea6b7cd74ec5c43fecb782a8fa0f050a3
Prashit Prakhar <prashitprakhar@yahoo.com>
2018-10-18 11:59:08 +0530

    Styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,30 @@
 //Import Libraries to make a component
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 //Make a component
 const Header = () => {
-    const { textStyle } = styles;
-    return <Text style={textStyle}>Register</Text>;
+    const { textStyle, viewStyle } = styles;
+return (
+    <View style={viewStyle}>
+    <Text style={textStyle}>Register</Text>
+    </View>
+);
 }
 
 const styles = {
+    viewStyle : {
+        backgroundColor : '#F8F8F8',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        paddingTop: 25,
+        shadowColor: '#000000',
+        shadowOffset: { width: 0, height : 2},
+        shadowOpacity: 0.2
+    },
     textStyle : {
-        fontSize : 50
+        fontSize : 30
     }
 }
 
