998a632806bcfa47ce39beab38c98606cb7bf54e
Prashit Prakhar <prashitprakhar@yahoo.com>
2018-10-19 10:32:15 +0530

    Updating for Album

---
+++ src/components/album-body.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './album-details';
+
+// const RegistrationBody = () => {
+//     return <Text>Album Body</Text>;
+// }
+
+class AlbumBody extends Component {
+
+    state = { albums: [] };
+
+    componentWillMount() {
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => this.setState({ albums: response.data }));
+    }
+
+    renderAlbums() {
+        return this.state.albums.map(album => {
+            return <AlbumDetail key={album.title} dataPassed={album} />
+            //return (<Text key={album.title}>{album.title}</Text>);
+        });
+    }
+
+    render() {
+        //return <Text>Album Body</Text>;
+        //console.log("rendering albums : ",this.state)
+        return (
+            <View>
+                {this.renderAlbums()}
+            </View>
+        );
+    }
+}
+
+export default AlbumBody;
